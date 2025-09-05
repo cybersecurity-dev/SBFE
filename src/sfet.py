9acf0c8be270d7f3dd175fb0bc188fe634aa3b90
cybersecurity-dev <174958202+cybersecurity-dev@users.noreply.github.com>
2025-09-05 17:36:10 +0200

    Update sfet.py
    ++added print_banner function

--- src/sfet.py
+++ src/sfet.py
@@ -2,10 +2,14 @@
 # -*- coding: UTF-8 -*-
 
 from pyfiglet import Figlet
-
-if __name__ == '__main__':
-
+def print_banner():
     f_b = Figlet(font='big')
     print(f_b.renderText('SFET'))
     f_s = Figlet(font='slant')
     print(f_s.renderText('Static Feature Extraction Tool for Potentially Malicious OS Files'))
+
+def main():
+    print_banner()
+
+if __name__ == "__main__":
+    main()
